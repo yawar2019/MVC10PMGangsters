9726263f4ff83cbd958311b97fd060afceb80244
yawar2019 <yawarali17@gmail.com>
2022-02-03 23:10:01 +0530

    committed

--- MVC10PMGangsters/Controllers/DefaultController.cs
+++ MVC10PMGangsters/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
 ﻿using MVC10PMGangsters.Models;
+using MVC10PMGangsters.MyCustomFilter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,8 @@ using System.Web.Mvc;
 
 namespace MVC10PMGangsters.Controllers
 {
+    [MyFilter]
+
     public class DefaultController : Controller
     {
         // GET: Default
@@ -14,13 +17,22 @@ namespace MVC10PMGangsters.Controllers
         {
             return View();
         }
+
         public ActionResult Index2(EmployeeModel emp)
         {
+            
             return View();
         }
 
         public ActionResult Index3(List<EmployeeModel> emp)
         {
+            return View();
+        }
+
+        [MyFilter]
+        public ActionResult CricketPremi()
+        {
+            ViewBag.Player ="Brain Lara";
             return View();
         }
     }

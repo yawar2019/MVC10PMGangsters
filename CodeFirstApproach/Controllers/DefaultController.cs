4a123f9127914fddee19e379fb6ae54141227348
yawar2019 <yawarali17@gmail.com>
2022-02-23 23:02:16 +0530

    committed

--- CodeFirstApproach/Controllers/DefaultController.cs
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using CodeFirstApproach.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +15,71 @@ namespace CodeFirstApproach.Controllers
         public ActionResult Index()
         {
             return View(db.EmployeeModels.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(employeeDetails emp)
+        {
+            db.EmployeeModels.Add(emp);//create query or insert query 
+            int i = db.SaveChanges();//execute query
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            employeeDetails emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(employeeDetails emp)
+        {
+            db.Entry(emp).State=EntityState.Modified;//Update query 
+            int i = db.SaveChanges();//execute query
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            employeeDetails emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            employeeDetails emp = db.EmployeeModels.Find(id);
+
+            db.EmployeeModels.Remove(emp);//Delete query 
+            int i = db.SaveChanges();//execute query
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View();
+
+            }
         }
     }
 }

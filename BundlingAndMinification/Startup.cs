8dfc67c0b3eff5f12aa81ee533fceff800bebebc
yawar2019 <yawarali17@gmail.com>
2022-03-12 23:27:05 +0530

    committed

---
+++ BundlingAndMinification/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BundlingAndMinification.Startup))]
+namespace BundlingAndMinification
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MVC10PMGangsters/scripts/samplescript.js
@@ -0,0 +1,2 @@
+﻿const person = { fname: "John", lname: "Doe", age: 25 }; var txt = ""; for (let t in person) txt += person[t] + " "; alert(txt), $("#demo").val(txt);
+ 
